21c9f4e1586762719880d9b5e11c361861d70c27
Weronika Bednarz <weronikaabednarz@gmail.com>
2024-02-23 18:03:48 +0100

    Creating and adding other types of widgets

--- data_entry_form.py
+++ data_entry_form.py
@@ -9,7 +9,7 @@ frame.pack()
 
 # saving user info
 user_info_frame = tkinter.LabelFrame(frame, text = "User Information")
-user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 20)
+user_info_frame.grid(row = 0, column = 0, padx = 20, pady = 10)
 
 first_name_label = tkinter.Label(user_info_frame, text = "First name")
 first_name_label.grid(row = 0, column = 0)
@@ -38,11 +38,44 @@ nationality_combobox.grid(row = 3, column = 1)
 
 gender_label = tkinter.Label(user_info_frame, text = "Gender")
 gender_label.grid(row = 2, column = 2)
-gender_spinbox = tkinter.Spinbox(user_info_frame, values = ["woman", "man"])
+gender_spinbox = ttk.Combobox(user_info_frame, values = ["woman", "man"])
 gender_spinbox.grid(row = 3, column = 2)
 
 for widget in user_info_frame.winfo_children():
     widget.grid_configure(padx = 10, pady = 5)
 
+# saving course info                            sticky - expand
+courses_frame = tkinter.LabelFrame(frame)
+courses_frame.grid(row = 1, column = 0, sticky = "news", padx = 20, pady = 10)
 
+registered_label = tkinter.Label(courses_frame, text = "Registration Status")
+registered_label.grid(row = 0, column = 0)
+registered_check = tkinter.Checkbutton(courses_frame, text = "Currently Registered")
+registered_check.grid(row = 1, column = 0)
+
+numcourses_label = tkinter.Label(courses_frame, text = "# Completed Courses")
+numcourses_label.grid(row = 0, column = 1)
+numcourses_spinbox = tkinter.Spinbox(courses_frame, from_ = 0, to = "infinity")
+numcourses_spinbox.grid(row = 1, column = 1)
+
+numsemesters_label = tkinter.Label(courses_frame, text = "# Semesters")
+numsemesters_label.grid(row = 0, column = 2)
+numsemesters_spinbox = tkinter.Spinbox(courses_frame, from_ = 0, to = "infinity")
+numsemesters_spinbox.grid(row = 1, column = 2)
+
+for widget in courses_frame.winfo_children():
+    widget.grid_configure(padx = 10, pady = 5)
+
+# accept terms
+terms_frame = tkinter.LabelFrame(frame, text = "Terms & Conditions")
+terms_frame.grid(row = 2, column = 0, sticky = "news", padx = 20, pady = 10)
+terms_check = tkinter.Checkbutton(terms_frame, text = "I accept all terms and conditions.")
+terms_check.grid(row = 0, column = 0)
+
+# button
+button_enter = tkinter.Button(frame, text = "Enter data")
+button_enter.grid(row = 3, column = 0, sticky = "e", padx = 20, pady = 10)
+
+button_cancel = tkinter.Button(frame, text = "Cancel")
+button_cancel.grid(row = 3, column = 0, sticky = "w", padx = 20, pady = 10)
 window.mainloop()
